572945e646731a4cc9e4c5e2485004b962eda4d9
Eduard Gilmutdinov <edgilmutdinov@gmail.com>
2017-03-15 20:27:34 +0500

    Add baseurl to the routers

--- src/containers/root/Router.js
+++ src/containers/root/Router.js
@@ -2,11 +2,16 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import App from '../app/App';
 
+const basename = process.env.PUBLIC_URL || '';
+
 const RoutedApp = () => (
   <Router>
     <Switch>
-      <Route path="/face/:eyes/:nose/:mouth/:color" component={App} />
-      <Route exact path="/" component={App} />
+      <Route
+        path={`${basename}/face/:eyes/:nose/:mouth/:color`}
+        component={App}
+      />
+      <Route exact path={`${basename}/`} component={App} />
       <Route render={() => <h1>Page not found</h1>} />
     </Switch>
   </Router>
